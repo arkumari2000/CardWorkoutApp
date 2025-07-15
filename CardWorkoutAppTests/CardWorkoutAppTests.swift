6379194f2684df5ec657c6f4b11dcdde8719ea8e
Archana Kumari <arkumari2000@gmail.com>
2025-07-15 20:13:06 +0530

    Initial Commit

---
+++ CardWorkoutAppTests/CardWorkoutAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  CardWorkoutAppTests.swift
+//  CardWorkoutAppTests
+//
+//  Created by Himanshu Bhati on 15/07/25.
+//
+
+import Testing
+@testable import CardWorkoutApp
+
+struct CardWorkoutAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
