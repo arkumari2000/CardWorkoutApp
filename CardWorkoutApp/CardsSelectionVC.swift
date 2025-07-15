5d8968c14da7a5126c8806c91ae7a37f2e1afd4f
Archana Kumari <arkumari2000@gmail.com>
2025-07-15 21:00:36 +0530

    Intial Setup of project

--- CardWorkoutApp/CardsSelectionVC.swift
+++ CardWorkoutApp/CardsSelectionVC.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  CardsSelectionVC.swift
 //  CardWorkoutApp
 //
 //  Created by Himanshu Bhati on 15/07/25.
@@ -7,13 +7,12 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class CardsSelectionVC: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+
+        view.backgroundColor = .magenta
     }
-
-
+    
 }
-
